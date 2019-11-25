a74a756e0dcac6efc7dff46a6c133ee1b003a598
wanghao9027 <279601149@qq.com>
2019-11-25 20:32:24 +0800

    my

--- 智能生态家居馆-------新/pages/di/me/me.js
+++ 智能生态家居馆-------新/pages/di/me/me.js
@@ -7,60 +7,25 @@ Page({
   data: {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+//事件
+  onDiscounts(){
+  wx.navigateTo({
+    url:"/pages/my/discounts/discounts"
+  })
+},
+  onCollect() {
+    wx.navigateTo({
+      url: "/pages/my/collect/collect"
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  onHistory() {
+    wx.navigateTo({
+      url: "/pages/my/history/history"
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onService() {
+    wx.navigateTo({
+      url: "/pages/my/service/service"
+    })
   }
 })
